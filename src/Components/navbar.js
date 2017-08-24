e67cdf0b6fbadc1e33ebec827a6ecef968578556
peteryunkim <peteryunkim@gmail.com>
2017-08-24 14:27:00 -0400

    almost done with navbar

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -4,16 +4,16 @@ const Navbar = () => {
 	return (
 		<div className='navbar'>
 		<ul className='list'>
-		<li className='list-item'><img className ='logo' src='https://www.inrhythm.com/wp-content/uploads/2017/06/logo.png'/></li>
+		<li className='list-item'><img className='logo' alt='logo' src='https://www.inrhythm.com/wp-content/uploads/2017/06/logo.png'/></li>
 		<li className='list-item'>CLIENTS </li> 
 		<li className='list-item'>TESTIMONIALS </li>
 		<li className='list-item'>SERVICES </li>
-		<li className='list-item'>OUR METHODOLOGY </li>
+		<li className='list-item'><span id='active-item'>OUR METHODOLOGY</span> </li>
 		<li className='list-item'>ABOUT US </li>
 		<li className='list-item'>BLOG </li>
 		<li className='list-item'>CAREERS </li>
 		<li className='list-item'>RESOURCES </li>
-		<li className='list-item'><button>CONTACT US</button></li>
+		<li className='list-item'><button id='contact'>CONTACT US</button></li>
 		</ul>
 		</div>
 	)
