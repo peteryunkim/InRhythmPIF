bd102268616cbabb958e20519cbe86879fda4037
peteryunkim <peteryunkim@gmail.com>
2017-08-24 15:55:01 -0400

    finished navbar and picture components

--- src/Components/pictureInfo.js
+++ src/Components/pictureInfo.js
@@ -3,7 +3,8 @@ import React from 'react'
 const PictureInfo = () => {
 	return(
 		<div className='pictureInfo'>
-		Picture
+			<h1 id='title'>Work Ethic Flow</h1>
+			<h4 id='title-subtext'> A glimpse into the mind of Peter Kim as he journeys through the world of programming.</h4>
 		</div>
 	)
 }
