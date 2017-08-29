8d4f5c047d7915e14b90540c92fc26b332353be4
peteryunkim <peteryunkim@gmail.com>
2017-08-29 16:36:05 -0400

    finished footer

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
 			<div className='footer-column'><img className='footer-logo' alt='logo' src='https://www.inrhythm.com/wp-content/uploads/2017/06/logo.png'/>
 				<h4 id='footer-col1'>InRhythm is a high-growth, New York based, technology consulting firm focused on high velocity software development through lean UX, software engineering and agile delivery services.</h4>
 			</div>
-			<div className='footer-column'>
+			<div className='footer-column footer-col2'>
 				<h4 className='footer-column-title'>Our Offices</h4>
 				<h4>140 Broadway, Suite 2270 New York, NY 10005</h4>
 			</div>
-			<div className='footer-column'>
+			<div className='footer-column footer-col3'>
 			<h4 className='footer-column-title'>Need help?</h4>
 				<h4><a href='https://www.inrhythm.com/blog/' className='links'>Our Blog</a></h4>
 				<h4><a href='https://www.inrhythm.com/contact-us/' className='links'>Contact Us</a></h4>
@@ -18,7 +18,7 @@ const Footer = () => {
 			</div>
 			<div className='footer-column'>
 				<h4 className='footer-column-title'>Get in touch</h4>
-				<ul>
+				<ul className='footer-ul'>
 					<li className='footer-icons'><a href='https://www.github.com/peteryunkim' className="links fa fa-github"> </a></li>
 					<li className='footer-icons'><a href='https://www.linkedin.com/in/peteryunkim' className="links fa fa-linkedin"> </a></li>
 					<li className='footer-icons'><a href='https://www.github.peteryunkim.io' className="links fa fa-odnoklassniki"> </a></li>
