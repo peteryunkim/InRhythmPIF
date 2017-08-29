d07eff885c6c9c0048ce6ec2da2e5c0635ca36b4
peteryunkim <peteryunkim@gmail.com>
2017-08-29 13:00:09 -0400

    working on footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Components/navbar'
 import PictureInfo from './Components/pictureInfo'
+import Footer from './Components/footer'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <Navbar/>
       <PictureInfo/>
       <div>Workflow</div>
+      <Footer/>
       </div>
     );
   }
---
+++ src/Components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+	return(
+		<div id='footer'>
+			<div className='footer-column'>column 1</div>
+			<div className='footer-column'>column 2</div>
+			<div className='footer-column'>column 3</div>
+			<div className='footer-column'>column 4</div>
+		</div>
+	)
+}
+
+export default Footer
