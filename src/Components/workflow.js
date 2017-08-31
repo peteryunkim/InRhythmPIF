3c6a55db3072f256ff64035cf773efa99f94c308
peteryunkim <peteryunkim@gmail.com>
2017-08-31 16:00:50 -0400

    finishing workflow

--- src/Components/workflow.js
+++ src/Components/workflow.js
@@ -3,14 +3,14 @@ import React from 'react'
 const Workflow = () => {
 return(
 	<div id='workflow'>
-		<div id='workflow-desc'>This workflow shows how Peter takes a problem and approaches it. It goes through complicated thoughts and considerations as system design, data management, and user experience are important.</div>
+		<div id='workflow-desc'>This workflow shows how Peter takes a problem and approaches it. It may not display everything that goes on in his head as this page would probably be populated with SO much junk and questions but it gives a general overview of the process. There are lots of thoughts and considerations as far as system design, data management, and user experience goes. Not only are his inputs important but also the inputs of others around him.</div>
 		<div id='workflow-btn'>
 			<div id='workflow-btn-text'>Start</div>
 		</div>
 		<div className='workflow-arrows'>
 			<div className="fa fa-arrow-down"></div>
 		</div>
-		<h3 id='phase-title'>Phase 1</h3>
+		<h1 id='phase-title'>Phase 1</h1>
 		<ul className='workflow-list'>
 			<li className='workflow-list-item3'>
 				<img className='workflow-icon'src={require('../images/person-icon.png')}/>
@@ -29,7 +29,7 @@ return(
 			<div className="fa fa-arrow-down"></div>
 			<div className="fa fa-arrow-down"></div>
 		</div>
-		<h3 id='phase-title'>Phase 2</h3>
+		<h1 id='phase-title'>Phase 2</h1>
 		<ul className='workflow-list'>
 			<li className='workflow-list-item3'>
 				<img className='workflow-icon'src={require('../images/glasses.png')}/>
@@ -43,9 +43,29 @@ return(
 		<div className='workflow-arrows'>
 			<div className="fa fa-arrow-down"></div>
 			<div className="fa fa-arrow-down"></div>
+			<h3>Think carefully, do you need to do more? If so, go back to phase 2.</h3>
+			<div className="fa fa-arrow-down"></div>
+			<div className="fa fa-arrow-down"></div>
+			<h3>It's the final countdown!</h3>
+			<div className="fa fa-arrow-down"></div>
+			<div className="fa fa-arrow-down"></div>
 			<div className="fa fa-arrow-down"></div>
 		</div>
-		<h3 id='phase-title'>Phase 3</h3>
+		<h1 id='phase-title'>Phase 3</h1>
+		<ul className='workflow-list'>
+			<li className='workflow-list-item5'>
+				<img className='workflow-icon'src={require('../images/checklist.png')}/>
+			</li>
+			<li className='workflow-list-item5'><img className='workflow-icon'src={require('../images/community.png')}></img></li>
+			<li className='workflow-list-item5'><img className='workflow-icon'src={require('../images/discussion.png')}></img></li>
+			<li className='workflow-list-item5'><img className='workflow-icon'src={require('../images/study.png')}></img></li>
+			<li className='workflow-list-item5'><img className='workflow-icon'src={require('../images/finish.png')}></img></li>
+			<li className='workflow-captions3'><h3>Get things done</h3></li>
+			<li className='workflow-captions3'><h3>Community discussion</h3></li>
+			<li className='workflow-captions3'><h3>Exchange feedback</h3></li>
+			<li className='workflow-captions3'><h3>Work on feedback</h3></li>
+			<li className='workflow-captions3'><h3>Finish</h3></li>
+		</ul>
 		<div className="fa fa-arrow-down"></div>
 		<div id='workflow-btn'><div id='workflow-btn-text'>Build</div></div>
 	</div>
