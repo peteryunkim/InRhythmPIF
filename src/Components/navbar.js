1a41042e10d3146f14fafecea817ce4af99dcb2c
peteryunkim <peteryunkim@gmail.com>
2017-09-01 13:58:00 -0400

    finished product

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -5,14 +5,14 @@ const Navbar = () => {
 		<div className='navbar'>
 		<ul className='list'>
 		<li className='list-item'><a href='https://www.inrhythm.com'><img className='logo' alt='logo' src='https://www.inrhythm.com/wp-content/uploads/2017/06/logo.png'/></a></li>
-		<li className='list-item'>CLIENTS </li> 
-		<li className='list-item'>TESTIMONIALS </li>
-		<li className='list-item'>SERVICES </li>
+		<li className='list-item'><a href='https://www.inrhythm.com/clients/'>CLIENTS</a> </li> 
+		<li className='list-item'><a href='https://www.inrhythm.com/testimonials/'>TESTIMONIALS</a> </li>
+		<li className='list-item'><a href='https://www.inrhythm.com/services/'>SERVICES</a> </li>
 		<li className='list-item'><span id='active-item'>OUR METHODOLOGY</span> </li>
-		<li className='list-item'>ABOUT US </li>
-		<li className='list-item'>BLOG </li>
-		<li className='list-item'>CAREERS </li>
-		<li className='list-item'>RESOURCES </li>
+		<li className='list-item'><a href='https://www.inrhythm.com/about/'>ABOUT US</a> </li>
+		<li className='list-item'><a href='https://www.inrhythm.com/blog/'>BLOG</a> </li>
+		<li className='list-item'><a href='https://www.inrhythm.com/jobs-career/'>CAREERS</a> </li>
+		<li className='list-item'><a href='https://www.inrhythm.com/resources/'>RESOURCES</a> </li>
 		<li className='list-item'><button id='contact'>CONTACT US</button></li>
 		</ul>
 		</div>
